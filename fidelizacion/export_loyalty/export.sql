827ee24ad1842c0688406dc6269b6366ffcafd15
rickmarina <rickmarina@gmail.com>
2017-10-24 12:58:28 +0200

    201710242

--- fidelizacion/export_loyalty/export.sql
+++ fidelizacion/export_loyalty/export.sql
@@ -1,22 +1,16 @@
-/* Script exportación datos fidelización LOYALTY 
-
+/* 
+Script exportación datos fidelización LOYALTY 
 
 Tabla de clientes
 Tabla de cuentas
 Tabla de tarjetas
-Tabla de transacciones de dos años (tanto 14fi, 16fi_h, tcf67) .
-Tabla de canjes (tcf49) 
-
+Tabla de transacciones de dos años (tanto 14fi, 16fi_h, tcf67).
+Tabla de canjes (tcf49)
 Datos del  Histórico, junio 2015 – junio 2017
-
-
-Hola Pilar, 
 
 Te envío este correo como propuesta formal para la construcción de un script que exporte los datos 
 requeridos por DISA para su envío a la empresa LOYALIA.
-
 Los datos se extraerán de la réplica de Conexflow alojada en DISA. 
-
 Se extraerán datos de las siguientes tablas y bajo el criterio indicado a continuación: 
 
 Tabla de clientes: 
@@ -35,19 +29,10 @@ Tabla de canjes (tcf49) (junio 2015 – junio 2017):
 Tal y como hemos realizado en otras ocasiones, por ejemplo con deloitte, 
 cada exportación generará un fichero de texto plano (dat) compuesto por los campos concatenados 
 usando el literal "$&_&$".
-
-El tiempo estimado para este trabajo sería de 3 días.
-
-Un saludo,
-Ricardo
-
 */
-
-
 
 sqlcmd -d CF_DISA -Q "SELECT * FROM VW_DQ_Clientes" -S SIECISAPRO01 -U US_CF_GYT -P Gytki01 -f 65001 -s"$&_&$" -W -o ".\clientes.dat"
 sqlcmd -d CF_DISA -Q "SELECT * FROM VW_DQ_TransaccionesFI" -S SIECISAPRO01 -U US_CF_GYT -P Gytki01 -f 65001 -s"$&_&$" -W -o ".\transacciones.dat"
 sqlcmd -d CF_DISA -Q "SELECT * FROM VW_DQ_Transacciones_Desglose" -S SIECISAPRO01 -U US_CF_GYT -P Gytki01 -f 65001 -s"$&_&$" -W -o ".\transacciones_desglose.dat"
-
 
 pause
